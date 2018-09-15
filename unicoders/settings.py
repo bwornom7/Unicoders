857d479321fef8c3487d739c88fb0833b8a73e00
shenr124 <shenr124@students.bju.edu>
2018-09-15 13:19:30 -0400

    Requirements

--- unicoders/settings.py
+++ unicoders/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -132,3 +133,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 SASS_PROCESSOR_ROOT = STATIC_URL[1:]
 
 LOGIN_REDIRECT_URL = '/'
+
+django_heroku.settings(locals())
