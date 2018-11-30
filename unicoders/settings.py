cb037a6e690efba3d2923b29122a21c1b19c68c0
Sam Henry <shenr124@students.bju.edu>
2018-11-30 09:18:44 -0500

    Debug False

--- unicoders/settings.py
+++ unicoders/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$b!jfycxk4c(rc1!r02*1qr&ww!_shafpkwb*kr_)3^xp6dok&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
